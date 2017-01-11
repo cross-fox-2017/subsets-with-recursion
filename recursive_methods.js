648257e881bcf78586a959ef52d202befb4b8af2
unknown <yaumashf@gmail.com>
2017-01-11 16:36:02 +0700

    done

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,7 +1,40 @@
-let choose_team = (n, k) => {
-  //write your code here
+// kombinasi adalah susunan dari unsur-unsur yang ditentukan dan tidak memperharikan urutannya
+// misalkan kita ingin mengambil 3 kelereng dengan berbagai warna pada sebuah tempat yang berisi warna merah,
+// putih, kuning, hijau, biru, ungu, hitam. kira-kira berapa kombinasi warna kelerang yang akan kita dapat?
+// hal ini dapat ditentukan dengan kombinasi, yaitu C(6,3)=6!/3!(6-3)!
+//pada javascript, kita dapat membuat program kombinasi sebagai berikut...
+
+function faktor_range(a,b) { //faktorial dari 'a' hingga 'b'
+  let x = b;
+  while (x < a) {
+    x++;
+    b*=x;
+  }
+  return b;
 }
 
-console.log(choose_team(6, 2));
-console.log(choose_team(6, 3));
-console.log(choose_team(24, 4));
+let choose_team = (n,k) => {
+  if (n==k) {
+    return 1;
+  }
+  else {
+    if (k < n-k) {
+      k = n-k
+      // console.log('k kurang 'k);
+    }
+    else {
+      k = k
+      // console.log('sama dg '+k);
+    }
+    return faktor_range(n,k+1)/faktor_range(n-k,1);
+  }
+}
+
+console.log(faktor_range(4,1));//24
+console.log(faktor_range(4,2));//24
+console.log(faktor_range(4,3));//12
+console.log(faktor_range(4,4));//1
+console.log(choose_team(6,6));//1
+console.log(choose_team(6,2));//15
+console.log(choose_team(6,3));//20
+console.log(choose_team(24,4));//10626
