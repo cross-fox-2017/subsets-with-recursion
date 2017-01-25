0e4e1a8e5e615f6683b027338a935f78ad389547
yomaswn <yaumashf@gmail.com>
2017-01-25 17:33:42 +0700

    done

--- recursive_methods.js
+++ recursive_methods.js
@@ -4,36 +4,41 @@
 // hal ini dapat ditentukan dengan kombinasi, yaitu C(6,3)=6!/3!(6-3)!
 //pada javascript, kita dapat membuat program kombinasi sebagai berikut...
 
-function faktor_range(a,b) { //faktorial dari 'a' hingga 'b'
-  let x = b;
-  while (x < a) {
-    x++;
-    b*=x;
-  }
-  return b;
-}
+// function faktor_range(a,b) { //faktorial dari 'a' hingga 'b'
+//   let x = b;
+//   while (x < a) {
+//     x++;
+//     b*=x;
+//   }
+//   return b;
+// }
 
 let choose_team = (n,k) => {
-  if (n==k) {
-    return 1;
-  }
-  else {
-    if (k < n-k) {
-      k = n-k
-      // console.log('k kurang 'k);
+  if (n>=k) {
+    if(n == k || k == 0){
+      return 1
     }
     else {
-      k = k
-      // console.log('sama dg '+k);
+       return choose_team(n-1,k)+choose_team(n-1,k-1)
     }
-    return faktor_range(n,k+1)/faktor_range(n-k,1);
   }
+  // else {
+  //   if (k < n-k) {
+  //     k = n-k
+  //     // console.log('k kurang 'k);
+  //   }
+  //   else {
+  //     k = k
+  //     // console.log('sama dg '+k);
+  //   }
+  //   return faktor_range(n,k+1)/faktor_range(n-k,1);
+  // }
 }
 
-console.log(faktor_range(4,1));//24
-console.log(faktor_range(4,2));//24
-console.log(faktor_range(4,3));//12
-console.log(faktor_range(4,4));//1
+// console.log(faktor_range(4,1));//24
+// console.log(faktor_range(4,2));//24
+// console.log(faktor_range(4,3));//12
+// console.log(faktor_range(4,4));//1
 console.log(choose_team(6,6));//1
 console.log(choose_team(6,2));//15
 console.log(choose_team(6,3));//20
